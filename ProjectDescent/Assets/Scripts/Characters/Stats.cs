552c7b7e8e31fd6aaa8488d14127e6b4a2750d4f
Deyton Henry <deyton37@gmail.com>
2017-05-17 18:36:42 -0600

    Changed this from CharacterBase to Stats

--- ProjectDescent/Assets/Scripts/Characters/Stats.cs
+++ ProjectDescent/Assets/Scripts/Characters/Stats.cs
@@ -1,11 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
-public class Stats : MonoBehaviour {
+public class Stats : MonoBehaviour, INotifyPropertyChanged{
 
-	// Use this for initialization
-	void Start () {
+    public event PropertyChangedEventHandler PropertyChanged;
+    private float health = 5F;
+    private float attackDamage = 0.5F;
+    private int moveSpeed = 1;
+    private float armour;
+    private Weapon weapon;
+
+    public float Health
+    {
+        get { return health; }
+        set
+        {
+            health = value;
+        }
+    }
+    public float AttackDamage
+    {
+        get { return attackDamage; }
+        set
+        {
+            attackDamage = value;
+        }
+    }
+    public int MoveSpeed
+    {
+        get { return moveSpeed; }
+        set
+        {
+            moveSpeed = value;
+        }
+    }
+    public float Armour
+    {
+        get { return armour; }
+        set
+        {
+            armour = value;
+        }
+    }
+
+
+    public float DealDamage()
+    {
+        return AttackDamage;
+    }
+    public void TakeDamage(float takenDamage)
+    {
+        Health = Health - takenDamage;
+    }
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
