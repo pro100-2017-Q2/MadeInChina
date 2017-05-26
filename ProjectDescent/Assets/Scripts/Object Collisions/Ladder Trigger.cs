20ab821d0b1ad1aa9493a732def4dd7c13ec0034
Zach Tovey <zachtovey05@gmail.com>
2017-05-26 17:31:33 -0600

    committing my object collision code

---
+++ ProjectDescent/Assets/Scripts/Object Collisions/Ladder Trigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderTrigger : MonoBehaviour {
+
+        void OnLadderTrigger(Collider col)
+        {
+            //Spawn play on a new map
+            
+        }
+    }
