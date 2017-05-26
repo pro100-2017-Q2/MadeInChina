f6327ca9cb16c438bf5e5485281d36905f9240f8
Jesse Stoltz <jstoltz@student.neumont.edu>
2017-05-26 17:53:19 -0600

    Started working on the UI.

---
+++ ProjectDescent/Assets/Scripts/UI/PauseGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour {
+    //public Transform menu;
+    public RectTransform menu;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+	}
+    public void Pause()
+    {
+        if(menu.gameObject.activeInHierarchy == false)
+        {
+            menu.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            menu.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
