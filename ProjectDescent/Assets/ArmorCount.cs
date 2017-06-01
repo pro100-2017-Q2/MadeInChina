6deef8f83c4e02276c1125e1ef9648e965336e1b
Jesse Stoltz <jstoltz@student.neumont.edu>
2017-06-01 17:11:26 -0600

    For Kevin

---
+++ ProjectDescent/Assets/ArmorCount.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArmorCount : MonoBehaviour {
+    public Text armorCount;
+	// Use this for initialization
+	void Start () {
+        armorCount = GetComponent<Text>();
+        DisplayArmorCount();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DisplayArmorCount();
+	}
+    public void DisplayArmorCount()
+    {
+        armorCount.text = "Armor: " + LevelController.player.armour.ToString();
+    }
+}
